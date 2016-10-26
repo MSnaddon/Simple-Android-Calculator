a528e63c3222bd62a9fc3a4210bc0ae03ebcbb68
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-26 19:15:39 +0100

    Tests written

--- app/src/main/java/com/example/user/zonalcalculator/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.example.user.zonalcalculator;
-
-/**
- * Created by user on 26/10/2016.
- */
-public class Calculator {
-
-    Operators operator;
-}
--- app/src/test/java/com/example/user/zonalcalculator/CalculatorTest.java
+++ app/src/test/java/com/example/user/zonalcalculator/CalculatorTest.java
@@ -3,20 +3,43 @@ package com.example.user.zonalcalculator;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 /**
  * Created by user on 26/10/2016.
  */
 public class CalculatorTest {
 
     Calculator calculator;
+    double n1;
+    double n2;
 
     @Before
     public void Before(){
+        n1 = 3.6;
+        n2 = 0.6;
         calculator = new Calculator();
     }
 
     @Test
     public void calculatorCanAdd(){
+        assertEquals((n1+n2), calculator.calculate(n1,n2, Operators.ADD),0.01);
+    }
 
-    };
+    @Test
+    public void calculatorCanSubtract(){
+        assertEquals((n1-n2), calculator.calculate(n1,n2, Operators.SUBTRACT), 0.01);
+    }
+
+    @Test
+    public void calculatorCanMultiply(){
+        assertEquals((n1*n2),calculator.calculate(n1,n2, Operators.MULTIPLY), 0.01);
+    }
+
+    @Test
+    public void calculatorCanDivide(){
+        assertEquals((n1/n2),calculator.calculate(n1,n2, Operators.DIVIDE), 0.01);
+    }
+
+
 }
