a81e12d8b3a1fac97b51686f21b85e77b6a0745c
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-26 19:27:20 +0100

    enum Tests written

---
+++ app/src/main/java/com/example/user/zonalcalculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.example.user.zonalcalculator;
+
+/**
+ * Created by user on 26/10/2016.
+ */
+public class Calculator {
+}
---
+++ app/src/test/java/com/example/user/zonalcalculator/OperatorsTest.java
@@ -0,0 +1,21 @@
+package com.example.user.zonalcalculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by user on 26/10/2016.
+ */
+public class OperatorsTest {
+
+    @Test
+    public void addOperatorActAccordingly(){
+        assertEquals( 6.5, Operators.ADD.execute(4,2.5), 0.01);
+        assertEquals( 1.5, Operators.SUBTRACT.execute(4,2.5), 0.01);
+        assertEquals( 10,Operators.MULTIPLY.execute(4,2.5), 0.01);
+        assertEquals( 2.5,Operators.DIVIDE.execute(5,2), 0.01);
+
+    }
+
+}
